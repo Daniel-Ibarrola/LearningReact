0677cdc8fe560ce54f0ae316fe17783a2a52a262
Daniel-Ibarrola <dis96051@gmail.com>
2023-04-18 09:16:40 -0600

    Lifting State

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,8 @@ const List = ({ list }) => (
 );
 
 const Search = ({ onSearch }) => {
-    const [searchTerm, setSearchTerm] = React.useState('');
 
     const handleChange = (event) => {
-        setSearchTerm(event.target.value);
         onSearch(event);
     }
 
@@ -29,12 +27,13 @@ const Search = ({ onSearch }) => {
         <div>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" onChange={handleChange}/>
-            <p>Searching for: <strong>{searchTerm}</strong></p>
         </div>
     )
 };
 
 const App = ()  => {
+
+    const [searchTerm, setSearchTerm] = React.useState('');
 
     const stories = [
         {
@@ -56,15 +55,19 @@ const App = ()  => {
     ];
 
     const handleSearch = (event) => {
-        console.log(event.target.value);
+        setSearchTerm(event.target.value);
     }
+
+    const searchedStories = stories.filter((story) =>
+       story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
     <div>
         <h1>My Hacker Stories</h1>
         <Search onSearch={handleSearch}/>
         <hr/>
-        <List list={stories}/>
+        <List list={searchedStories}/>
     </div>
     )
 };
