9a34216292eae34f7c59f713921ffb40c44628f8
Daniel-Ibarrola <dis96051@gmail.com>
2023-04-18 09:03:41 -0600

    Search uses state and callback handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,72 @@
 import * as React from "react";
 
+const Item = (props) => (
+    <li>
+        <span>
+            <a href={props.item.url}>{props.item.title}</a>
+        </span>
+        <span> {props.item.author}</span>
+        <span> {props.item.num_comments}</span>
+        <span> {props.item.points}</span>
+    </li>
+);
 
-const list = [
-    {
-        title: "React",
-        url: "https://reactjs.org",
-        author: "Jordan Walke",
-        num_comments: 3,
-        points: 4,
-        objectId: 0
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 5,
-      points: 5,
-      objectId: 1,
-    }
-]
-
-function List() {
-    return (
+const List = ({ list }) => (
         <ul>
-            {list.map(item =>
-                <li key={item.objectId}>
-                    <span>
-                        <a href={item.url}>{item.title}</a>
-                    </span>
-                    <span> {item.author}</span>
-                    <span> {item.num_comments}</span>
-                    <span> {item.points}</span>
-                </li>
-            )}
+            {list.map(item => <Item key={item.objectId} item={item}/>)}
         </ul>
-    )
-}
+);
 
-function Search() {
+const Search = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = React.useState('');
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+        onSearch(event);
+    }
+
     return (
         <div>
             <label htmlFor="search">Search: </label>
-            <input id="search" type="text" />
+            <input id="search" type="text" onChange={handleChange}/>
+            <p>Searching for: <strong>{searchTerm}</strong></p>
         </div>
     )
-}
+};
 
-function App() {
+const App = ()  => {
 
-  return (
+    const stories = [
+        {
+            title: "React",
+            url: "https://reactjs.org",
+            author: "Jordan Walke",
+            num_comments: 3,
+            points: 4,
+            objectId: 0
+        },
+        {
+          title: "Redux",
+          url: "https://redux.js.org",
+          author: "Dan Abramov, Andrew Clark",
+          num_comments: 5,
+          points: 5,
+          objectId: 1,
+        }
+    ];
+
+    const handleSearch = (event) => {
+        console.log(event.target.value);
+    }
+
+    return (
     <div>
         <h1>My Hacker Stories</h1>
-        <Search />
-        <hr />
-        <List />
+        <Search onSearch={handleSearch}/>
+        <hr/>
+        <List list={stories}/>
     </div>
-  );
-}
+    )
+};
 
-export default App
+export default App;
