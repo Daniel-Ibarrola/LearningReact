793a1426e715ec0d411853b8deacf2498b535ea7
Daniel-Ibarrola <dis96051@gmail.com>
2023-04-28 13:00:48 -0600

    Search uses form

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,24 @@ const InputWithLabel = ({
         </>
     );
 
+
+const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
+     <form onSubmit={onSearchSubmit}>
+             <InputWithLabel
+                id="search"
+                value={searchTerm}
+                isFocused
+                onInputChange={onSearchInput}
+             >
+                <strong>Search: </strong>
+            </InputWithLabel>
+            <button type="submit" disabled={!searchTerm}>
+                Submit
+            </button>
+    </form>
+);
+
+
 const useStorageState = (key, initialState) => {
 
     const [value, setValue] = React.useState(
@@ -128,7 +146,7 @@ const App = ()  => {
         } catch {
             dispatchStories({type: failFetchStory});
         }
-        
+
     }, [url]);
 
     React.useEffect(() => {
@@ -146,26 +164,19 @@ const App = ()  => {
        });
     };
 
-    const handleSearchSubmit = () => {
+    const handleSearchSubmit = (event) => {
         setUrl(`${API_ENDPOINT}${searchTerm}`);
+        event.preventDefault();  // prevent browser reload
     }
 
     return (
     <div>
         <h1>My Hacker Stories</h1>
-        <InputWithLabel
-            id="search"
-            value={searchTerm}
-            isFocused
-            onInputChange={handleSearchInput}
-        >
-            <strong>Search: </strong>
-        </InputWithLabel>
-        <button type="button"
-                disabled={!searchTerm}
-                onClick={handleSearchSubmit}>
-            Submit
-        </button>
+        <SearchForm
+            searchTerm={searchTerm}
+            onSearchInput={handleSearchInput}
+            onSearchSubmit={handleSearchSubmit}
+        />
         <hr/>
         {stories.isError && <p>Something went wrong...</p>}
 
