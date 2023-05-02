17740c9608d68d1ccd3d7d2bbb017b038d15aa1b
Daniel-Ibarrola <dis96051@gmail.com>
2023-05-02 14:09:16 -0600

    Search component snapshot test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -153,6 +153,11 @@ describe("SearchForm", () => {
        expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
     });
 
+   it("renders snapshot", () => {
+      const { container } = render(<SearchForm {...searchFormProps}/>);
+      expect(container.firstChild).toMatchSnapshot();
+   });
+
 });
 
 describe("InputWithLabel", () => {
